4ca317e59ee63527d2d00feef0612cf32c98b5bc
eriveros <eriveros298@gmail.com>
2020-10-28 18:37:48 -0300

    adding changes

--- block_notas.js
+++ block_notas.js
@@ -1,70 +1,139 @@
 const RESOLVE="RESOLVE"
 const PENDANT="PENDANT"
 const dateSeparator="-"
-const NOTESFILE=".\\bloc_notas\\notes.json"
+const NOTESFILE=".\\notes.json"
 const fs= require('fs')
+
 
 let note_list=[]
 
-
+/** this method allow the initialization of the block_notas script. It will look in the
+ * current directory for the notes.json and look the persisted notes. In case that there isn't 
+ * any notes.json, it will initialize with defautls values. It 's execute before exporting the module
+ */
 function init(){
-    fs.readFile(NOTESFILE,'utf-8',(err,data)=>{
-        if(err) console.log(err)
-        console.log(typeof JSON.parse(data) )
-    })
-   /* const fd=fs.openSync(NOTESFILE,'r')
-    let fileInfo=fs.readFileSync(fd)
-    fs.closeSync(fd)*/
+    try {
+        let data=fs.readFileSync(NOTESFILE,'utf-8')
+        let objects=JSON.parse(data)
+        for (const iterator of objects) {
+            createAndAddNote(iterator)
+        }
+    } catch (error) {
+        if(error.code != "ENOENT"){
+            throw error
+        }else{
+            console.log("A new file will be created since it cannot be found")
+        }
+    }
 }
 
-function Note(title,description){
-    this.title=title
-    let date=new Date()
-    this.creatingDate=date.getFullYear() +dateSeparator+(date.getMonth()+1)+dateSeparator+(date.getDay()+1)
-    this.state=PENDANT
+/**Method that creates a note from an object */
+function createAndAddNote(object){
+    let {state,tittle,description,date}= object
+    if(tittle != undefined && tittle != null && tittle.length > 0){
+    note_list.push(new Note(tittle,description,date,state))
+    }
+}
+/**Constructor for Class Note */
+function Note(tittle,description="",date=formatedDate(),state=PENDANT){
+    this.tittle=tittle
     this.description=description
-}
-
-function markNoteAsResolve(note){
-    pendantNote=note_list.filter((x,note)=> x.title == note.title)
-    pendantNote.state=RESOLVE
-}
-
-function appendTofile(note){
-    let noteAsJson=JSON.stringify(note)
-    fs.appendFileSync(NOTESFILE,noteAsJson)
-}
-
-function save(){
-    let noteAsJson=JSON.stringify(note)
-    fs.writeFile(NOTESFILE, data, (err) => {
-        if (err) throw err;
-    });
-}
-
-function convert(){
-    let data=JSON.parse("fileContent")
-
-}
-function createNote(tittle,description){
-    let new_note=new Note(tittle,description)
-    note_list.push(new_note)
-}
-
-function listNotes(){
-    note_list.forEach(x=>{
+    this.state=state
+    this.date=date
+    this.toString=function (pretty){
+        if(pretty == undefined || isNaN(pretty) || pretty === null || pretty instanceof Number  || pretty instanceof String){
+            pretty=false
+        }
+        if(pretty){
         console.log("--------------------------------------------------")
-        console.log(x.creatingDate +"                       "+ x.title)
-        console.log(x.description)
-        console.log(x.state)
-    } )
-} 
+        console.log(this.date +"                       "+ this.tittle)
+        console.log(this.description)
+        console.log(this.state)
+        }else{
+            console.log(this.date +" | "+ this.tittle+" | "+this.description +" | "+ this.state)
+        }
+    }
+}
+/**Format the date to a specific format */
+function formatedDate(){
+    let date=new Date()
+    return date.getFullYear() +dateSeparator+(date.getMonth()+1)+dateSeparator+(date.getDay()+1)
+
+}
+
+
+/**Changes the state of a specific note
+ *  @param tittle unique id of note.Default value is ""
+ *  @param description description of a note. Default value is ""
+ */
+function markNoteAsResolve(tittle="",descripcion=""){
+    
+    for (const iterator of note_list) {
+        if(iterator.tittle == tittle || iterator.description == descripcion){
+            iterator.state=RESOLVE
+            return
+        }
+    }
+    console.log("No existen notas con los datos ingresados")
+}
+/**
+ * Persist the notes and the modifications in the notes.json file
+ */
+function saveToFile(){
+    let fileContent=JSON.stringify(note_list)
+    fs.writeFileSync(NOTESFILE,fileContent)
+}
+
+/**
+ * List all the nodes with pendant state.
+ * @param {*} pretty boolean value that allows to change to a pretty output. Default value is false
+ */
+function filterbyPendant(pretty=false){
+    let array=note_list.filter( x=>x.state==PENDANT)
+    if(array.length>0){
+        array.forEach( x =>x.toString(pretty))
+    }else{
+        console.log("No hay notas pendientes para listar")
+    }
+}
+
+/**
+ * Creates a note. If the tittle is undefined or null or empty it will throw an exception
+ * @param {*} tittle 
+ * @param {*} description 
+ */
+function createNote(tittle,description="No Description"){
+    
+    if(tittle !=undefined && tittle != null && tittle.trim().length == 0){
+        throw new Error("The tittle cannot be empty")
+    }
+    for (const iterator of note_list) {
+        if(iterator.tittle == tittle){
+            iterator.tittle=tittle
+            iterator.description=description
+            return
+        }
+    }
+    note_list.push(new Note(tittle,description))
+    }
+/**
+ * List all the notes 
+ * @param {*} pretty boolean value that allows to change to a pretty output. Default value is false
+ */
+function listNotes(pretty=false){
+    if(note_list.length>0){
+
+        note_list.forEach(x=>x.toString(pretty) )
+    }else{
+        console.log("No hay notas para listar")
+    }
+}
 
 init()
-
 module.exports={
-    save,
+    saveChanges:saveToFile,
     listNotes,
+    listPendantNotes:filterbyPendant,
     changeState:markNoteAsResolve,
     createNote,
 }
---
+++ index.js
@@ -0,0 +1,78 @@
+const PromptSync =require('prompt-sync');
+const prompt = require('prompt-sync')({sigint: true}); 
+const blockNotas= require('./block_notas')
+
+console.log("**************************************************")
+console.log("***                                           ****")
+console.log("***                 NOTE APP                  ****")
+console.log("***                                           ****")
+console.log("**************************************************")
+
+function menu_de_opciones(){
+    console.log("*** Seleccione alguna opcion                  ****")
+    console.log("***  1)Crear Nota                             ****")
+    console.log("***  2)Modificar Estado de la Nota            ****")
+    console.log("***  3)Listar Todas las Notas                 ****")
+    console.log("***  4)Listar Notas Pendientes                ****")
+    console.log("***  5)Guardar y Salir                        ****")
+}
+
+
+let opcion_incorrecta=false
+let opcion
+do{
+    menu_de_opciones()
+    opcion=prompt("Seleccione su opcion: ") 
+    console.log(" ") 
+    do{
+    opcion_incorrecta=false   
+    switch(opcion){
+        case "1" : crearNotaMenu();break
+        case "2" : moficarEstadoNotaMenu();break
+        case "3" : menuListarNotas();break
+        case "4" : menuListarNotasPendientes();break
+        case "5" : menuGuardarySalir();break
+        default: console.log("valor incorrecto de opcion. Por favor seleccione una opcion valida")
+                opcion=prompt("Seleccione su opcion: ")
+                opcion_incorrecta=true;break
+        }
+    }while(opcion_incorrecta)
+    console.log(" ")
+    console.log("**************************************************")
+    console.log(" ")
+    console.log(" ")
+}while( opcion !=5)
+function crearNotaMenu(){
+    let titulo=prompt("Ingrese el titulo de la nota: ")
+    if(titulo.length == 0){
+        console.log(" ")
+        console.log("El titulo no puede ser vacio")
+        return
+    }
+    let descripcion=prompt("Ingrese la descripcion de la nota: ")
+    blockNotas.createNote(titulo,descripcion)
+}
+function moficarEstadoNotaMenu(){
+    
+    let titulo=prompt("Ingrese el titulo de la nota que desea marcar como resuelta: ")
+    let descripcion
+    if(titulo.length ==0 ){
+        descripcion=prompt("Ingrese la descripcion de la nota que desea marcar como resuelta: ")
+        if(descripcion == 0 || descripcion == undefined){
+            console.log(" ")
+            console.log("Se debe ingresar el titulo o la descripcion para cambiar el estado de la nota")
+            return
+        }    
+    }
+    blockNotas.changeState(titulo,descripcion)
+    console.log("la nota: "   +titulo!=undefined?titulo:descripcion + " ha sido marcada como Resolve")
+}
+function menuListarNotas(){
+    blockNotas.listNotes() 
+}
+function menuListarNotasPendientes(){
+    blockNotas.listPendantNotes()
+}
+function menuGuardarySalir(){
+    blockNotas.saveChanges()
+}
